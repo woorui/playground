08c7dbe1366ed8d7172fa334adc35f6c9e16d34d
wurui <rui1009479218@gmail.com>
2022-03-01 15:39:26 +0800

    add gorm-cache test

--- go.mod
+++ go.mod
@@ -3,18 +3,17 @@ module gorm.io/playground
 go 1.16
 
 require (
-	github.com/denisenkom/go-mssqldb v0.10.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
-	github.com/jackc/pgx/v4 v4.11.0 // indirect
-	github.com/mattn/go-sqlite3 v1.14.7 // indirect
-	golang.org/x/crypto v0.0.0-20210505212654-3497b51f5e64 // indirect
-	golang.org/x/text v0.3.6 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gorm.io/driver/mysql v1.0.6
-	gorm.io/driver/postgres v1.1.0
-	gorm.io/driver/sqlite v1.1.4
-	gorm.io/driver/sqlserver v1.0.7
-	gorm.io/gorm v1.21.9
+	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
+	github.com/jackc/pgx/v4 v4.15.0 // indirect
+	github.com/lib/pq v1.10.4 // indirect
+	github.com/mattn/go-sqlite3 v1.14.12 // indirect
+	github.com/stretchr/objx v0.3.0 // indirect
+	github.com/stretchr/testify v1.7.0
+	github.com/woorui/gorm-cache v0.0.0-20220228064057-a80e0e6e38e5
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	gorm.io/driver/mysql v1.3.2
+	gorm.io/driver/postgres v1.3.1
+	gorm.io/driver/sqlite v1.3.1
+	gorm.io/driver/sqlserver v1.3.1
+	gorm.io/gorm v1.23.1
 )
-
-replace gorm.io/gorm => ./gorm
--- main_test.go
+++ main_test.go
@@ -1,20 +1,111 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"sync"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	gormcache "github.com/woorui/gorm-cache"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
-// GORM_BRANCH: master
+// GORM_BRANCH: gorm-cache-test
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	mdb := memdb()
 
-	DB.Create(&user)
+	DB.Use(gormcache.GormCache(mdb, time.Second, gormcache.Models(new(User))))
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	user := User{Name: "jz"}
+	tx := DB.Begin(&sql.TxOptions{Isolation: sql.LevelDefault})
+
+	tx.Create(&user)
+
+	tx.Create(&User{Name: "zj"})
+
+	var jz User
+	err := tx.Model(new(User)).Where("name=?", "jz").First(&jz).Error
+	if err != nil {
+		t.Fatal("error when call .First")
 	}
+
+	var users []User
+	err = tx.Model(new(User)).Order("id ASC").Find(&users).Error
+	if err != nil {
+		t.Fatal("error when call .Find")
+	}
+
+	var cacheUser User
+
+	ok, val, _ := mdb.Get(context.TODO(), "SELECT * FROM `users` WHERE name=\"jz\" AND `users`.`deleted_at` IS NULL ORDER BY `users`.`id` LIMIT 1")
+
+	assert.Equal(t, true, ok)
+
+	err = json.Unmarshal([]byte(val), &cacheUser)
+	if err != nil {
+		assert.Fail(t, "Unmarshal cache data error")
+	}
+
+	assert.Equal(t, "jz", cacheUser.Name)
+
+	var cacheUsers []User
+
+	ok, arr, _ := mdb.Get(context.TODO(), "SELECT * FROM `users` WHERE `users`.`deleted_at` IS NULL ORDER BY id ASC")
+
+	assert.Equal(t, true, ok)
+
+	err = json.Unmarshal([]byte(arr), &cacheUsers)
+	if err != nil {
+		assert.Fail(t, "Unmarshal cache data error")
+	}
+
+	assert.Equal(t, "jz", cacheUsers[0].Name)
+	assert.Equal(t, "zj", cacheUsers[1].Name)
+
+	tx.Commit()
+
+}
+
+type mdb struct {
+	data    *sync.Map
+	expires *sync.Map
+}
+
+func memdb() gormcache.CacheKV {
+	result := &mdb{
+		data:    &sync.Map{},
+		expires: &sync.Map{},
+	}
+	return result
+}
+
+func (kv *mdb) Get(ctx context.Context, key string) (bool, string, error) {
+	ex, ok := kv.expires.Load(key)
+	if ok {
+		ext := ex.(time.Time)
+		if ext.Before(time.Now()) {
+			kv.expires.Delete(key)
+			kv.data.Delete(key)
+			return false, "", nil
+		}
+		val, ok := kv.data.Load(key)
+		if ok {
+			return true, val.(string), nil
+		}
+	}
+	return false, "", nil
+}
+
+func (kv *mdb) Set(ctx context.Context, key string, value string, exp time.Duration) error {
+	fmt.Println("xxxx", key, value)
+	kv.data.Store(key, value)
+	kv.expires.Store(key, time.Now().Add(exp))
+	return nil
 }
